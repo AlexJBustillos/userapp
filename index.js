2b0f2b2ab3086b727a21282baf5041f92c764137
alexjbustillos <alexjbustillos@gmail.com>
2020-11-19 14:03:34 -0500

    [sequelize]

--- index.js
+++ index.js
@@ -32,9 +32,105 @@ const db = require('./models');
 //     console.log(numRowsChanged);
 // });
 
-//delete a user
-db.user.destroy({
-    where: { lastName: 'Bobby' }
-}).then(numRowsDeleted => {
-    console.log(numRowsDeleted);
-});
+// //delete a user
+// db.user.destroy({
+//     where: { lastName: 'Bobby' }
+// }).then(numRowsDeleted => {
+//     console.log(numRowsDeleted);
+// });
+
+// add pet to user
+// db.user.findOne().then((foundUser) => {
+//     foundUser.createPet({
+//         name: 'Kaida',
+//         species: 'Black Cat'
+//     })
+// })
+
+//get pet from user
+// db.user.findOne().then((foundUser) => {
+//     foundUser.getPets().then((foundPets) => {
+//         console.log(foundPets);
+//     })
+// })
+
+// get user from pet
+// db.pet.findOne({
+//     where: {
+//         name: 'Kaida'
+//     }
+// }).then((kaida) => {
+//     kaida.getUser().then((foundUser) => {
+//         console.log(foundUser);
+//     })
+// })
+
+//create pet without user
+// db.pet.create({
+//     name: 'Hercules',
+//     species: 'Chow chow'
+// }).then((herc) => {
+//     console.log(herc);
+// })
+
+//add herc to user
+// db.pet.findOne({
+//     where: {
+//         name: 'Hercules'
+//     }
+// }).then((herc) => {
+//     db.user.findOne().then((foundUser) => {
+//         herc.setUser(foundUser)
+//     })
+// })
+
+//create toy
+// db.toy.create({
+//     type: 'squeaky',
+//     color: 'red'
+// })
+
+//add toy to pebs
+// db.pet.findOne({
+//     where: {
+//         name: 'Pebs'
+//     }
+// }).then((pebs) => {
+//     db.toy.findOne({
+//         where: {
+//             color: 'red',
+//             type: 'squeaky'
+//         }
+//     }).then((foundToy) => {
+//         pebs.addToy(foundToy)
+//     })
+// })
+
+//add the same toy to kaida
+// db.pet.findOne({
+//     where: {
+//         name: 'Kaida'
+//     }
+// }).then((kaida) => {
+//     db.toy.findOne({
+//         where: {
+//             color: 'red',
+//             type: 'squeaky'
+//         }
+//     }).then((foundToy) => {
+//         kaida.addToy(foundToy)
+//     })
+// })
+
+//find all pets that share red squeaky toy
+db.toy.findOne({
+    where: {
+        type: 'squeaky',
+        color: 'red'
+    }
+}).then((foundToy) => {
+    foundToy.getPets().then((pets) => {
+        //render this to a page instead of console.log "pets"
+        console.log(pets);
+    })
+})
